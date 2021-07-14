08debd6fe9a854092f2c3384afe5e0ff60396b19
jthomps3 <56907018+jthomps3@users.noreply.github.com>
2021-07-14 10:30:17 -0500

    Add files via upload

---
+++ D1ReportRepair.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class D1ReportRepair {
+    
+    public static void main(String[] args) {     
+        // Read input file into a vector  
+        Vector <Integer> nums = new Vector<>(); 
+        try {
+            File myObj = new File("D1ReportRepair.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextInt()) {
+                nums.add(myReader.nextInt());                
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error has occurred");
+            e.printStackTrace();
+        }
+        
+        boolean found = false;
+        int num1 = 0, num2 = 0;
+
+        for (int i = 0; i < nums.size() - 1 && found == false; i++) {
+            for (int j = 0; j < nums.size() && found == false; j++) {
+                if (nums.elementAt(i) + nums.elementAt(j) == 2020) {
+                    num1 = nums.elementAt(i);
+                    num2 = nums.elementAt(j);
+                    found = true;
+                }
+            }
+        }
+        if (num1 == 0 && num2 == 0) {
+            System.out.println("No 2 numbers add up to 2020");
+            System.exit(0);
+        } else {
+            System.out.println("1st Number: " + num1 + "\n2nd Number: " + num2 + "\nResult = " + (num1 * num2));
+        }       
+    }
+}
