97575c885497fa30f04351e55b33751c1ba2e062
jthomps3 <56907018+jthomps3@users.noreply.github.com>
2021-07-15 18:50:13 -0500

    Add files via upload
    Added Part 2

--- D1ReportRepair.java
+++ D1ReportRepair.java
@@ -38,5 +38,30 @@ public class D1ReportRepair {
         } else {
             System.out.println("1st Number: " + num1 + "\n2nd Number: " + num2 + "\nResult = " + (num1 * num2));
         }       
+        
+        // Part 2
+        found = false;
+        int num3 = 0;
+        num1 = 0;
+        num2 = 0;
+        for (int i = 0; i < nums.size() && found == false; i++) {
+            for (int j = 0; j < nums.size() && found == false; j++) {
+               for (int k = 0; k < nums.size() && found == false; k++) {
+                   if (nums.elementAt(i) + nums.elementAt(j) + nums.elementAt(k) == 2020) {
+                        num1 = nums.elementAt(i);
+                        num2 = nums.elementAt(j);
+                        num3 = nums.elementAt(k);
+                        found = true;
+                   }
+               } 
+            }
+        }
+        if (num1 == 0 || num2 == 0 || num3 == 0) {
+            System.out.println("No 3 numbers add up to 2020");
+            System.exit(0);
+        } else {
+            System.out.println("1st Number: " + num1 + "\n2nd Number: " + num2 + "\n3rd Number: " + num3 +
+            "\nResult = " + (num1 * num2 * num3));
+        }
     }
 }
